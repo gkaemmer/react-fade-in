fdc2d6d6856b8542152ef6d2c164a261c33436db
Adam Moisa <adammoisa@gmail.com>
2017-06-08 11:02:31 +0300

    made sure lag value is int

--- src/FadeIn.js
+++ src/FadeIn.js
@@ -7,7 +7,7 @@ export default class FadeIn extends Component {
 
   componentDidMount() {
     const count = React.Children.count(this.props.children);
-    var customLag = this.props.lag ? parseInt(this.props.lag) : 50;
+    var customLag = parseInt(this.props.lag) ? parseInt(this.props.lag) : 50;
     let i = 0;
     this.interval = setInterval(() => {
       i++;
