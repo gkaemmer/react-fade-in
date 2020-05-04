5cfa8e0822429fe9d69e96da9effc6a389ca763f
Martin Belev <martinbelev93@gmail.com>
2020-05-04 20:49:08 +0300

    Migrate FadeIn to functional component

--- src/FadeIn.js
+++ src/FadeIn.js
@@ -1,55 +1,43 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
-export default class FadeIn extends Component {
-  constructor() {
-    super();
-    this.state = {
-      maxIsVisible: 0
-    };
-  }
+export default ({
+  children,
+  className,
+  childClassName,
+  delay = 50,
+  transitionDuration = 400,
+}) => {
+  const [maxIsVisible, setMaxIsVisible] = useState(0);
 
-  get delay() {
-    return this.props.delay || 50;
-  }
+  useEffect(() => {
+    const childrenCount = React.Children.count(children);
+    const interval = setInterval(() => {
+      setMaxIsVisible((count) => {
+        if (count >= childrenCount) clearInterval(interval);
 
-  get transitionDuration() {
-    return this.props.transitionDuration || 400;
-  }
+        return count + 1;
+      });
+    }, delay);
 
-  componentDidMount() {
-    const count = React.Children.count(this.props.children);
-    let i = 0;
-    this.interval = setInterval(() => {
-      i++;
-      if (i > count) clearInterval(this.interval);
+    return () => clearInterval(intervalID);
+  }, []);
 
-      this.setState({ maxIsVisible: i });
-    }, this.delay);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    const transitionDuration = this.transitionDuration;
-    return (
-      <div className={this.props.className}>
-        {React.Children.map(this.props.children, (child, i) => {
-          return (
-            <div
-              className={this.props.childClassName}
-              style={{
-                transition: `opacity ${transitionDuration}ms, transform ${transitionDuration}ms`,
-                transform: `translateY(${this.state.maxIsVisible > i ? 0 : 20}px)`,
-                opacity: this.state.maxIsVisible > i ? 1 : 0
-              }}
-            >
-              {child}
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={className}>
+      {React.Children.map(children, (child, i) => {
+        return (
+          <div
+            className={childClassName}
+            style={{
+              transition: `opacity ${transitionDuration}ms, transform ${transitionDuration}ms`,
+              transform: `translateY(${maxIsVisible > i ? 0 : 20}px)`,
+              opacity: maxIsVisible > i ? 1 : 0,
+            }}
+          >
+            {child}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
